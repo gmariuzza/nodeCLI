a6f9442bbeca4de7a39ad034afffc1c23d5dd6e9
Gregory Mariuzza <Gregory Mariuzza>
2017-06-24 11:19:14 -0500

    adding updated code1

--- weather-admin.js
+++ weather-admin.js
@@ -6,20 +6,39 @@ function newUserSearch(user, location){
     this.user = user;
     this.location = location;
     this.date = 
-    this.getWeather = function{
-    }
-}
-
-
-function WeatherAdmin()
-{
-    this.user=user,
-    this.location=locaiton,
-    this.newUserSearch=function(user,location)
+    this.getWeather = function(user,location)
     {
         weather.find({search: location, degreeType: 'F'}, function(err, result) {
   if(err) console.log(err);
  
   console.log(JSON.stringify(result, null, 2));
-});
-    },
+	});
+    }
+}
+
+
+function WeatherAdmin(){
+	this.readData = function(){
+		var fs = require("fs")
+		fs.readFile("log.txt", "utf8", function (err, data){
+			console.log(data);
+
+			})
+
+
+
+	}
+
+
+}
+var fs = require('fs');
+var lookup = {
+  total: function () {
+    fs.readFile('bank.txt', 'utf8', function (err, data) {
+      console.log("total:" + 
+      data
+        .split(',') // convert to array
+        .map(d => +d) // convert to decimals     
+        .reduce((a, c) => a + c) // add everything together
+        .toLocaleString('en-US', { style: 'currency', currency: 'USD' })); //format as currency
+    })
